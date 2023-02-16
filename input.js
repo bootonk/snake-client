02a44dc566305a7b5e353f04ec505dc9586faedf
Kate <115913025+bootonk@users.noreply.github.com>
2023-02-15 16:18:02 -0800

    Update files to connect user inputs and messages to the server

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
-// setup interface to handle user input from stdin
+let connection;
 
-const setupInput = function () {
+// setup interface to handle user input from stdin
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,9 +13,21 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
